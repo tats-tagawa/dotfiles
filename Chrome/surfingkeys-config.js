1d1ad1e42a3272166ce99a20e7a704b8877e8a65
Tats <10057207+tats-tagawa@users.noreply.github.com>
2023-03-07 23:09:32 -0800

    refactor: remove example configs

--- Chrome/surfingkeys-config.js
+++ Chrome/surfingkeys-config.js
@@ -29,17 +29,6 @@ api.Hints.style(
     box-shadow: 0px 0px;} \
 ', 'text');
 
-// an example to create a new mapping `ctrl-y`
-api.mapkey('<ctrl-y>', 'Show me the money', function() {
-    Front.showPopup('a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close).');
-});
-
-// an example to replace `T` with `gt`, click `Default mappings` to see how `T` works.
-api.map('gt', 'T');
-
-// an example to remove mapkey `Ctrl-i`
-api.unmap('<ctrl-i>');
-
 // set theme
 settings.theme = `
 .sk_theme {
