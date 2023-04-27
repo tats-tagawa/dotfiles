057f16da0cb4274163edcd1c3eb3d9d935defaca
Tats <10057207+tats-tagawa@users.noreply.github.com>
2023-04-26 22:39:44 -0700

    feat: use init.lua for vscode-nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,14 @@
 if vim.g.vscode then
+    -- set leader to <Space>
+    vim.g.mapleader = " "
+    local opts = {noremap = true, silent = true}
+    local set_key = vim.keymap.set
+    set_key("n", "<leader>w",
+            "<Cmd>call VSCodeCall('workbench.action.files.save')<CR>", opts)
+    set_key("n", "<leader>f",
+            "<Cmd>call VSCodeCall('editor.action.formatDocument')<CR>", opts)
 else
-  require("tats.keymaps")
-  require("tats.plugins")
-  require("tats.options")
+    require("tats.keymaps")
+    require("tats.plugins")
+    require("tats.options")
 end
