6e7cbd23a3c87e4a1008fb73f57e278a610fc3b9
Tats <10057207+tats-tagawa@users.noreply.github.com>
2023-05-04 11:48:41 -0700

    add telescope file ignore patterns

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,7 +1,21 @@
-local builtin = require("telescope.builtin")
+local builtin = require('telescope.builtin')
 local set_key = vim.keymap.set
 
-set_key("n", "<leader>ff", builtin.find_files, {})
-set_key("n", "<leader>fg", builtin.live_grep, {})
-set_key("n", "<leader>fb", builtin.buffers, {})
-set_key("n", "<leader>fh", builtin.help_tags, {})
+require 'telescope'.setup({
+  defaults = {
+    file_ignore_patterns = {
+      'tmux/plugins/*',
+      '%.pdf',
+      '%.mp3',
+      '%.jpg',
+      '%.png',
+      '%.DS_Store*',
+      '%.zip',
+      '%.dmg',
+    }
+  }
+})
+set_key('n', '<leader>ff', builtin.find_files, {})
+set_key('n', '<leader>fg', builtin.live_grep, {})
+set_key('n', '<leader>fb', builtin.buffers, {})
+set_key('n', '<leader>fh', builtin.help_tags, {})
