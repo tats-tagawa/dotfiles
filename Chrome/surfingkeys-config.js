d2a26c1955a8a7eaeb5903637c80aa9e2bd737b9
Tats <10057207+tats-tagawa@users.noreply.github.com>
2023-05-05 22:54:47 -0700

    more catppuccin

--- Chrome/surfingkeys-config.js
+++ Chrome/surfingkeys-config.js
@@ -1,44 +1,44 @@
-settings.scrollStepSize	= 400;
+settings.scrollStepSize = 400;
 settings.smoothScroll = true;
 settings.focusAfterClosed = 'last';
 settings.hintAlign = 'left';
 settings.tabsThreshold = 3;
 
 // set h/l to go back/forward in history
-api.map('h','S')
-api.map('l','D')
+api.map('h', 'S')
+api.map('l', 'D')
 
 // set H/L to go to the left/right tab of open tab 
-api.map('H','E')
-api.map('L','R')
+api.map('H', 'E')
+api.map('L', 'R')
 
 // set ctrl-j to switch to last viewed tab
-api.map('<Ctrl-j>','<Ctrl-6>')
+api.map('<Ctrl-j>', '<Ctrl-6>')
 
 // Follow link mode hints
 api.Hints.style(' \
-  font-family: system-ui, sans serif; \
+  font-family: ui-monospace, Menlo, monospace; \
   font-size: 14px; \
   font-weight: 700; \
-  border: solid 1px #000000; \
+  border: solid 1px #1E1E2E; \
   padding: 2px; \
-  color: #111111; \
+  color: #1E1E2E; \
   background: unset; \
-  background-color: #FFA500; \
+  background-color: #F2CDCD; \
   box-shadow: 0px 0px; \
 ');
 
 // Visual mode hints
 api.Hints.style(
   'div, div.begin { \
-    font-family: system-ui, sans serif; \
+    font-family: ui-monospace, Menlo, monospace; \
     font-size: 14px; \
     font-weight: 700; \
-    border: solid 1px #000000; \
+    border: solid 1px #1E1E2E; \
     padding: 2px; \
-    color: #111111; \
+    color: #1E1E2E; \
     background: unset; \
-    background-color: #E0E0E0; \
+    background-color: #CBA6F7; \
     box-shadow: 0px 0px;} \
 ', 'text');
 
@@ -46,47 +46,73 @@ api.Hints.style(
 // set theme
 settings.theme = `
 .sk_theme {
-    font-family: system-ui, Input Sans Condensed, Charcoal, sans-serif;
-    font-size: 12pt;
-    background: #24272e;
-    color: #abb2bf;
+    font-family: system-ui, sans-serif;
+    font-size: 14px;
+    background: #1e1e2e;
+    color: #A6ADC8;
+    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.5);
 }
+
 .sk_theme tbody {
     color: #fff;
 }
+
 .sk_theme input {
     color: #d0d0d0;
+    font-family: system-ui, sans-serif;
 }
+
 .sk_theme .url {
-    color: #61afef;
+    color: #89B4FA;
 }
+
 .sk_theme .annotation {
     color: #56b6c2;
 }
+
+.sk_theme .sk_omnibar_middle {
+    padding: 1rem;
+}
+
 .sk_theme .omnibar_highlight {
-    color: #528bff;
+    color: #F38BA8;
 }
+
 .sk_theme .omnibar_timestamp {
-    color: #e5c07b;
+    color: #F2CDCD;
 }
+
 .sk_theme .omnibar_visitcount {
-    color: #98c379;
+    color: #A6E3A1;
 }
+
 .sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
-    background: #303030;
+    background: #313244;
 }
+
+#sk_omnibarSearchArea .prompt {
+    display: none;
+}
+
+#sk_omnibarSearchResult li.focused div.url {
+    white-space: nowrap;
+}
+
 .sk_theme #sk_omnibarSearchResult ul li.focused {
-    background: #3e4452;
+    background: #45475A;
 }
+
 #sk_omnibarSearchArea .prompt, #sk_omnibarSearchArea .resultPage, #sk_omnibarSearchArea>input {
-    font-size: 20px;
+    font-size: 16px;
     font-weight: 700;
-    padding-bottom: 5px;
+    padding: 0px 0px 5px 0px;
 }
+
 #sk_omnibarSearchArea .prompt .separator {
     padding-left: 5px;
 }
+
 #sk_status, #sk_find {
     font-size: 20pt;
-}`;
-// click `Save` button to make above settings to take effect.</ctrl-i></ctrl-y>
+}
+`;
