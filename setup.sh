e7a658a639a8a9122e1ea4e7d22ec9de19af7efc
Tats <10057207+tats-tagawa@users.noreply.github.com>
2023-05-07 16:25:23 -0700

    create setup script

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/zsh
+
+# Install homebrew, formulaes and casks
+
+echo "Checking if homebrew installed..."
+
+if test ! $(which brew)
+then
+  echo "Homebrew not found..."
+  echo "Installing Homebrew for you"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "Homebrew already installed"
+fi
+
+formulaes=(
+  coreutils # Don't forget to add gnubin/gnuman to PATH
+  curl
+  ffmpeg
+  gnutls
+  lua
+  lua-language-server
+  luajit
+  luarocks
+  neovim
+  nvm
+  python
+  ripgrep
+  tmux
+  trash
+  tree
+  tree-sitter
+  yt-dlp
+  zsh-vi-mode
+)
+
+echo "Installing formulaes..."
+brew install ${formulaes[@]}
