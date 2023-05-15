fb46f868f7f2f198f9c77425ce18604a7782c970
Tats <10057207+tats-tagawa@users.noreply.github.com>
2023-05-15 14:57:23 -0700

    add nvim keymaps

--- nvim/lua/tats/keymaps.lua
+++ nvim/lua/tats/keymaps.lua
@@ -4,6 +4,10 @@ vim.g.mapleader = " "
 local opts = { noremap = true, silent = true }
 local set_key = vim.keymap.set
 
+----------------
+--- general ----
+----------------
+
 -- use JK as escape
 set_key("i", "jk", "<esc>", opts)
 set_key("v", "jk", "<esc>", opts)
@@ -11,6 +15,22 @@ set_key("v", "jk", "<esc>", opts)
 -- save and quit shortcuts
 set_key("n", "<leader>w", ":w<cr>", opts)
 set_key("n", "<leader>q", ":wq<cr>", opts)
+
+-- refresh file
+set_key("n", "<leader>ee", ":e!<cr>")
+
+-- don't join with <s-j>
+set_key("n", "<s-j>", "<nop>")
+
+-- move current line up/down
+set_key("n", "-", "ddp")
+set_key("n", "_", "ddkP")
+
+----------------
+--- toggles ----
+----------------
+
+set_key("n", "<leader>n", ":set relativenumber!<cr>")
 
 ----------------
 --- buffer ----
@@ -29,8 +49,10 @@ set_key("n", "<s-j><s-j>", ":bprev<cr>", opts)
 -- go to previous buffer
 set_key("n", "<C-h>", ":e #<cr>", opts)
 
--- netrw
--- set_key("n", "<leader>pv", vim.cmd.Ex, opts)
+-------------------
+--- navigation ----
+-------------------
+
 -- nvim-tree
 set_key("n", "<leader>nt", ":NvimTreeToggle<cr>", opts)
 
