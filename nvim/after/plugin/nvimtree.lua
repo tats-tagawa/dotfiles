7ac46a2c7c9a9ae623f7a9104973aef78f33d1bb
Tats <10057207+tats-tagawa@users.noreply.github.com>
2023-06-20 22:10:21 -0700

    enable float for nvim-tree

--- nvim/after/plugin/nvimtree.lua
+++ nvim/after/plugin/nvimtree.lua
@@ -5,14 +5,23 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
 -- OR setup with some options
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
     width = 30,
+    float = {
+      enable = true,
+      quit_on_focus_loss = false,
+      open_win_config = {
+        relative = "editor",
+        border = "rounded",
+        width = 30,
+        height = 30,
+        row = 1,
+        col = 1,
+      },
+    },
   },
   renderer = {
     group_empty = true,
